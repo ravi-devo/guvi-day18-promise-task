1d64456338910ad774cd621a394e9ca6aac996cb
ravi-devo <ravi.ravichandran370@gmail.com>
2023-09-29 09:57:42 +0530

    Indian mutual fund api fetched and displayed the data

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const endpointsArray = [
     url: "https://ergast.com/api/f1/drivers.json?"
   },
   {
-    name: "",
-    url: ""
+    name: "Indian mutual funds",
+    url: "https://api.mfapi.in/mf"
   },
   {
     name: "",
@@ -13,35 +13,37 @@ const endpointsArray = [
   }
 ];
 
-for(let i = 0; i < endpointsArray.length; i++){
+for (let i = 0; i < endpointsArray.length; i++) {
   fetchData(endpointsArray[i].name, endpointsArray[i].url);
 }
 
-async function fetchData(name, url){
+async function fetchData(name, url) {
   try {
     const response = await fetch(url);
     const jsonData = await response.json();
-    if(name == "F1 formula racing"){
+    if (name == "F1 formula racing") {
       f1FormulaRacing(jsonData);
+    } else if (name == "Indian mutual funds") {
+      indianMutualFundDetails(jsonData);
     }
   } catch (error) {
     console.log("Error: ", error);
   }
 }
 
-function f1FormulaRacing(jsonData){
+function f1FormulaRacing(jsonData) {
   const f1Drivers = jsonData.MRData.DriverTable.Drivers;
   let counter = 1;
   for (const {
-          "url": biographyURL,
-          "givenName": name,
-          "familyName": lastName,
-          "dateOfBirth": dOB,
-          "nationality": nationality
-        }
-          of f1Drivers) {
-          const tableRow = document.createElement('tr');
-          tableRow.innerHTML = `
+    "url": biographyURL,
+    "givenName": name,
+    "familyName": lastName,
+    "dateOfBirth": dOB,
+    "nationality": nationality
+  }
+    of f1Drivers) {
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
           <td>${counter}</td>
           <td>${name}</td>
           <td>${lastName}</td>
@@ -49,8 +51,33 @@ function f1FormulaRacing(jsonData){
           <td>${nationality}</td>
           <td><a href="${biographyURL}" target="_blank">${name}'s biography</a></td>
           `;
-          const classSelector = document.querySelector('.data-flow');
-          classSelector.appendChild(tableRow);
-          counter++;
-        }
+    const classSelector = document.querySelector('.data-flow');
+    classSelector.appendChild(tableRow);
+    counter++;
+  }
 }
+
+function indianMutualFundDetails(jsonData) {
+  const mutualFundDetails = jsonData;
+  let counter = 1;
+  for (const {
+    "schemeCode": schemeCode,
+    "schemeName": schemeName
+  } of mutualFundDetails) {
+    if(counter <= 30){
+      const tableRow = document.createElement('div');
+      tableRow.innerHTML = `
+      <table class="table table-bordered table-dark w-50">
+      <thead><th>Scheme Code</th><th>Scheme Name</th></thead>
+      <tbody><tr><td>${schemeCode}</td><td>${schemeName}</td>
+      </table>
+      `
+      const classSelector = document.querySelector('.indian-mutualFunds');
+      classSelector.appendChild(tableRow);
+      counter++;
+    }else{
+      break;
+    }
+  }
+}
+
